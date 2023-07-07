93b65d215d3029c208abacc201dc2d66402214cf
uday krishna <udaykrishna5@gmail.com>
2023-07-07 21:13:36 +0530

    070723 changes

--- js/index.js
+++ js/index.js
@@ -27,49 +27,25 @@ window.endDate_ = endDate;
 // Ready
 
 export function generate(){
-    
-// let output = `{
-// "gameCategory": "BULL_VS_BEAR",
-// "opensAt": ${openDate.lastSelectedDate.valueOf()},
-// "closesAt": ${closeDate.lastSelectedDate.valueOf()},
-// "endsAt": ${endDate.lastSelectedDate.valueOf()},
-// "metadata": {
-//     "winCategory": "${document.getElementById('winCategory').value}"
-// },
-// "fee": "${document.getElementById('fee').value}",
-// "entities":{},
-// "options": [
-//     {
-//         "optType": "ENTITY",
-//         "dataType": "STRING",
-//         "value": "IND50",
-//         "entity": "IND::IND50||PRICE"
-//     },
-//     {
-//         "optType": "ENTITY",
-//         "dataType": "STRING",
-//         "value": "INDBANK",
-//         "entity": "IND::INDBANK||PRICE"
-//     }
-// ]
-// }`
+let status = document.getElementById('status');
+status.innerText = "";
+
 let stock = document.getElementById('stock');
-let output = `
-{
+let formOutput = `{
     "gameCategory": "CLASSIC",
     "opensAt": ${openDate.lastSelectedDate.valueOf()},
     "closesAt": ${closeDate.lastSelectedDate.valueOf()},
     "endsAt": ${endDate.lastSelectedDate.valueOf()},
     "metadata":{
-        "color": "RED"
+        "color": "${document.getElementById("color").value}"
     },
-    "fee":"1",
+    "fee":"${document.getElementById("fee").value}",
     "entities":{
         "e1":{
                 "id": ${stock.value},
-                "name":"${stock.selectedOptions[0].innerText}",
+                "name":"${window.stockdataClassic[stock.value].name}",
                 "source": "IND",
-                "ticker":"${stock.selectedOptions[0].innerText}",
+                "ticker":"${window.stockdataClassic[stock.value].stockTicker}",
                 "valueType":"PRICE"
             }
     },
@@ -86,30 +62,64 @@ let output = `
         }
     ]
 }`
-document.getElementById("code").innerHTML=output;
+document.getElementById("code").innerHTML=formOutput;
+
+fetch("https://dev.jito.club/api/v1/game/create", {
+    mode: "cors",
+    method: "PUT",
+    headers:{
+        "Content-Type": "application/json",
+        "X-Auth-Token": `${window.authKey}`
+    },
+    body:formOutput
+    }).then(resp=>resp.text().then(data=>status.innerText=`${resp.status} \n ${data}`));
+
 }
+
 
 window.generate = generate;
-async function makeStocks(gameType){
-    resp = await fetch("https://dev.jito.club/api/v1/game/entities",
-      {
-      method: "POST",
-  		mode: "cors",
-      body: JSON.stringify({"select":"CLASSIC"}),
-  		headers:{
-        	"Content-Type": "application/json",
-            "X-Auth-Token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..fQFp03eoantQsAcp.0-QE6lvO78f5JdaHhYXuw6EuCjuH4QJ6UuhGSka5aQiiNkqV_5hsQnlLyx8XVX7WNtXjVolF92h-AAVA5Euoug.OQu95wDC7sxUAdi4EUkEYA"
-      },
-      redirect: 'follow'
-      }
-     );
-     stocks = await resp.json();
-     let options = ''
-     for (let x of stocks){
-        options += `<option value="${x.id}">${x.name}</option>`;
-     }
-     document.getElementById("stock").innerHTML=options;
+
+
+
+async function makeStocks(){
+    let resp = await fetch("https://dev.jito.club/api/v1/game/entities", {
+            mode: "cors",
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "X-Auth-Token": `${window.authKey}`
+            },
+            body:JSON.stringify({"select":"CLASSIC"})
+            });
+    let data = await resp.json();
+    window.stockdataClassic = {};
+    let stockSelect = ''
+    for(let entry of data){
+        stockSelect += `<option value="${entry.id}">${entry.name}</option>`
+        window.stockdataClassic[entry.id] = entry;
+    }
+    document.getElementById("stock").innerHTML = stockSelect;
 }
 
-makeStocks();
+window.makeStocks = makeStocks;
+// window..then(console.log(""))
 
+
+async function punchKey(){
+    document.getElementById("userDetails").innerHTML = "";
+    let key = document.getElementById("authToken").value;
+    let resp = await fetch("https://dev.jito.club/api/v1/user/profile", {
+        mode: "cors",
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json",
+            "X-Auth-Token": `${key}`
+        }
+        });
+    let data = await resp.text()
+    document.getElementById("userDetails").innerHTML = `${resp.status} \n ${data}`;
+    window.authKey = key;
+    await makeStocks();
+}
+
+window.punchKey = punchKey;
